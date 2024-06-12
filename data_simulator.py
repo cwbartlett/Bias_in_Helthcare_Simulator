da8a98d3265ae36380c3e8089c2974c81b045ab4
Bartlett Lab <cwbartlett@users.noreply.github.com>
2024-06-12 16:09:21 -0400

    Update for more comprehensive bias model

--- data_simulator.py
+++ data_simulator.py
@@ -1,17 +1,21 @@
 import numpy as np
 import pandas as pd
+from scipy.stats import norm
 
 class HealthDataSimulator:
-    def __init__(self, num_samples, feature_details, bias_config, groups_config, disease_config):
+    def __init__(self, num_samples, feature_details, bias_config, groups_config, disease_config, correlated_config):
         self.num_samples = num_samples
         self.feature_details = feature_details
         self.bias_config = bias_config
         self.groups_config = groups_config
         self.disease_config = disease_config
+        self.correlated_config = correlated_config
         self.data = pd.DataFrame()
 
     def generate_data(self):
-        # Generate unbiased data
+        np.random.seed(0)  # Ensure reproducibility
+
+        # Generate basic features
         for feature, f_type in self.feature_details.items():
             if f_type == 'numerical':
                 self.data[feature] = np.random.normal(loc=50, scale=10, size=self.num_samples)
@@ -23,14 +27,32 @@ class HealthDataSimulator:
         # Assign groups
         self.data['Group'] = np.random.choice(list(self.groups_config.keys()), size=self.num_samples, p=[1/len(self.groups_config)]*len(self.groups_config))
 
+        # Generate disease state and correlated variables
+        self.generate_disease_state()
+        self.generate_correlated_variables()
+
         # Introduce biased variables
         self.introduce_bias()
 
-        # Generate disease state
-        self.generate_disease_state()
-
         # Censor data
         self.censor_data()
+
+    def generate_disease_state(self):
+        self.data['DiseaseLiability'] = np.random.normal(loc=0, scale=1, size=self.num_samples)
+        for group, params in self.groups_config.items():
+            group_indices = self.data[self.data['Group'] == group].index
+            self.data.loc[group_indices, 'DiseaseLiability'] += np.random.normal(loc=params.get('disease_mean_shift', 0), scale=params.get('disease_variance_factor', 1), size=len(group_indices))
+        prevalence_threshold = np.percentile(self.data['DiseaseLiability'], 100 - self.disease_config['prevalence'])
+        self.data['DiseaseState'] = (self.data['DiseaseLiability'] >= prevalence_threshold).astype(int)
+
+    def generate_correlated_variables(self):
+        for var_name, var_config in self.correlated_config.items():
+            mean = var_config.get('mean', 0)
+            std = var_config.get('std', 1)
+            corr = var_config['correlation']
+
+            base_var = np.random.normal(loc=mean, scale=std, size=self.num_samples)
+            self.data[var_name] = self.data['DiseaseLiability'] * corr + base_var * np.sqrt(1 - corr**2)
 
     def introduce_bias(self):
         for bias_type, settings in self.bias_config.items():
@@ -40,23 +62,17 @@ class HealthDataSimulator:
                     bias_name = f"{bias_type}_biased_{i+1}_{group}"
                     if bias_type == 'numerical':
                         base_data = self.data.loc[group_indices, settings['base_feature']] + np.random.normal(loc=params['mean_shift'], scale=params['variance_factor'], size=len(group_indices))
+                        self.data.loc[group_indices, bias_name] = base_data
                     elif bias_type == 'ordinal':
                         base_probs = np.array([0.1, 0.2, 0.4, 0.2, 0.1]) * params['variance_factor']
                         normalized_probs = base_probs / base_probs.sum()
                         base_data = np.random.choice([1, 2, 3, 4, 5], size=len(group_indices), p=normalized_probs)
+                        self.data.loc[group_indices, bias_name] = base_data
                     elif bias_type == 'categorical':
                         base_probs = np.array([0.3, 0.4, 0.3]) * params['variance_factor']
                         normalized_probs = base_probs / base_probs.sum()
                         base_data = np.random.choice(['low', 'medium', 'high'], size=len(group_indices), p=normalized_probs)
-                    self.data.loc[group_indices, bias_name] = base_data
-
-    def generate_disease_state(self):
-        self.data['DiseaseLiability'] = np.random.normal(loc=0, scale=1, size=self.num_samples)
-        for group, params in self.groups_config.items():
-            group_indices = self.data[self.data['Group'] == group].index
-            self.data.loc[group_indices, 'DiseaseLiability'] += np.random.normal(loc=params.get('disease_mean_shift', 0), scale=params.get('disease_variance_factor', 1), size=len(group_indices))
-        prevalence_threshold = np.percentile(self.data['DiseaseLiability'], 100 - self.disease_config['prevalence'])
-        self.data['DiseaseState'] = (self.data['DiseaseLiability'] >= prevalence_threshold).astype(int)
+                        self.data.loc[group_indices, bias_name] = base_data
 
     def censor_data(self):
         drop_indices = []
@@ -68,6 +84,12 @@ class HealthDataSimulator:
                 censor_prob = self.smooth_censoring_function(income, education, params['censor_shape'])
                 if np.random.rand() < censor_prob:
                     drop_indices.append(index)
+            # Censoring based on new variables
+            for var_name in self.correlated_config.keys():
+                censor_prob = params.get(f'censor_{var_name}_probability', 0)
+                for index in group_indices:
+                    if np.random.rand() < censor_prob:
+                        drop_indices.append(index)
 
         self.data_censored = self.data.drop(index=drop_indices).reset_index(drop=True)
         if len(self.data_censored) > self.num_samples:
--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from data_simulator import HealthDataSimulator
 from utils import save_data_to_csv, summarize_data, plot_data_distribution, plot_biased_scatter, plot_disease_liability_histograms
 
 def main():
-    if len(sys.argv) < 4:
-        print("Usage: python main.py '<bias_config>' '<groups_config>' '<disease_config>'")
+    if len(sys.argv) < 5:
+        print("Usage: python main.py '<bias_config>' '<groups_config>' '<disease_config>' '<correlated_config>'")
         sys.exit(1)
 
-    # Parse the JSON strings for bias configuration, groups configuration, and disease configuration from command-line arguments
+    # Parse the JSON strings for bias configuration, groups configuration, disease configuration, and correlated variables configuration from command-line arguments
     bias_config = json.loads(sys.argv[1])
     groups_config = json.loads(sys.argv[2])
     disease_config = json.loads(sys.argv[3])
+    correlated_config = json.loads(sys.argv[4])
 
     # Define basic feature details that will be used to generate unbiased data
     feature_details = {
@@ -21,7 +22,7 @@ def main():
     }
 
     # Initialize the simulator with the number of samples, feature details, and bias configuration
-    simulator = HealthDataSimulator(num_samples=10000, feature_details=feature_details, bias_config=bias_config, groups_config=groups_config, disease_config=disease_config)
+    simulator = HealthDataSimulator(num_samples=1000, feature_details=feature_details, bias_config=bias_config, groups_config=groups_config, disease_config=disease_config, correlated_config=correlated_config)
     simulator.generate_data()
     data = simulator.get_data()
     data_censored = simulator.get_censored_data()
